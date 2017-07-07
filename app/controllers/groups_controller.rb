f306c08c69aaf4fb29147d719872250c40e5fec9
谭宇 <tanyucn@outlook.com>
2017-07-07 16:49:16 +0800

    destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,6 +33,13 @@ class GroupsController < ApplicationController
 
   end
 
+    def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      flash[:alert] = "删除成功"
+      redirect_to groups_path
+    end
+
   private
 
   def group_params
