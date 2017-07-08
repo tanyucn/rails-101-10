3515a10e2eae36fc3bfea00400b794588d36b928
谭宇 <tanyucn@outlook.com>
2017-07-08 17:20:30 +0800

    自定义文章 编辑 删除功能]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,8 +20,34 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  
+    redirect_to groups_path, alert: "Group deleted"
+  end
+
 
   private
+
+  def find_group_and_check_permission
+    @post = Post.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
